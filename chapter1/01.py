b336abf6cff5a34434209e649f5efd7a88397ac2
aha-oretama <sekine_y_529@msn.com>
2019-04-17 00:19:32 +0900

    Refactor 01

--- chapter1/01.py
+++ chapter1/01.py
@@ -1,11 +1,6 @@
 def extract_odd(text):
     str_list = list(text)
-    odds = []
-    for i in range(len(str_list)):
-        if i % 2 == 0:
-            continue
-        odds.append(str_list[i])
-    return "".join(odds)
+    return "".join([ str_list[i] for i in range(len(str_list)) if i % 2 != 0])
 
 
 print(extract_odd("パタトクカシーー"))
